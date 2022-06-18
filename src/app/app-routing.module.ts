35a0fb0187fbe38dd82b0dff6aa64cf3fd44df3d
Kaan Karaman <25592633+kaan35@users.noreply.github.com>
2022-06-18 17:58:55 +0300

    feat(articles): added article archive page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
 import { ArticleComponent } from './pages/article/article.component';
+import { ArchiveComponent } from './pages/archive/archive.component';
 
 const routes: Routes = [
   { path: 'index', component: IndexComponent },
   { path: 'article/:slug', component: ArticleComponent },
+  { path: 'archive', component: ArchiveComponent },
   { path: '', component: IndexComponent },
 ];
 
